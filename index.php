a1f8947830e16d48e8f748d45ac89c27e7cffc19
richa-2001 <trciha623@gmail.com>
2021-05-09 13:02:29 +0530

    My project

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title> Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div class="row intro py-1" style="background-color :teal;">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+
+                  <h1> WELCOME TO SPARK BANK</h1>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md img text-center">
+              
+              </div>
+            </div><br><br>
+            <br>
+
+      <!-- Activity section -->
+            <div class="row activity text-center">
+                  <div class="col-md act">
+                    <img src="img/user.jpg" class="img-fluid" >
+                    <br>
+                    <a href="createuser.php"><button style="background-color :teal;">Create a User</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/paisa.jpg" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button style="background-color :teal;">Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/xyz.jpg" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><button style="background-color :teal;">Transaction History</button></a>
+                  </div>
+            </div>
+      </div>
+     </body>
+</html>
